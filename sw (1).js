5b4c260cab4039c2e5501aa8780c8df1d6c007ca
L3beet <brandontristan603@gmail.com>
2026-06-25 20:30:56 -0500

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'l3-v1';
+const FILES = [
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
